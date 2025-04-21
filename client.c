19d94d2a373f0aeac49b44feaee98a501017d83b
RobinsKarani <karanithiaine4@gmail.com>
2025-04-21 23:54:21 +0300

    Add file transfer logic and expiry metadata sending to client

--- client.c
+++ client.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,11 +10,10 @@
 #define BUFFER_SIZE 4096
 #define MAX_FILE_SIZE (10 * 1024 * 1024)
 
-
-
-
-int main(int argc, char *argv[]) {
-    if (argc != 4) {
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
         printf("Usage: %s <file> <server_ip> <expire_secs>\n", argv[0]);
         return 1;
     }
@@ -26,14 +23,16 @@ int main(int argc, char *argv[]) {
     int expire_in = atoi(argv[3]);
 
     FILE *fp = fopen(filename, "rb");
-    if (!fp) {
+    if (!fp)
+    {
         perror("File open failed");
         return 1;
     }
 
     struct stat st;
     stat(filename, &st);
-    if (st.st_size > MAX_FILE_SIZE) {
+    if (st.st_size > MAX_FILE_SIZE)
+    {
         printf("ERROR: File size exceeds 10MB.\n");
         fclose(fp);
         return 1;
@@ -42,13 +41,36 @@ int main(int argc, char *argv[]) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in serv_addr = {
         .sin_family = AF_INET,
-        .sin_port = htons(PORT)
-    };
+        .sin_port = htons(PORT)};
     inet_pton(AF_INET, server_ip, &serv_addr.sin_addr);
 
-    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
         perror("Connection failed");
         fclose(fp);
         return 1;
     }
-} 
+
+    char *base = strrchr(filename, '/');
+    base = base ? base + 1 : filename;
+
+    send(sock, base, 256, 0);
+
+    uint32_t size_net = htonl((uint32_t)st.st_size);
+    send(sock, &size_net, sizeof(size_net), 0);
+
+    uint32_t expire_net = htonl((uint32_t)expire_in);
+    send(sock, &expire_net, sizeof(expire_net), 0);
+
+    char buffer[BUFFER_SIZE];
+    size_t n;
+    while ((n = fread(buffer, 1, BUFFER_SIZE, fp)) > 0)
+    {
+        send(sock, buffer, n, 0);
+    }
+
+    printf("[+] File uploaded: %s (expires in %d secs)\n", base, expire_in);
+    fclose(fp);
+    close(sock);
+    return 0;
+}
