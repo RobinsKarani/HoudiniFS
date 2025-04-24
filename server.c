31403c74ac12c6150fe1a1c682cb2e791f4969cb
Habiba Hassan <146443955+Ladyhabz1@users.noreply.github.com>
2025-04-24 22:16:08 +0300

    implement basic file upload with expiry and auto-deletion thread

--- server.c
+++ server.c
@@ -22,62 +22,6 @@ void save_expiry(const char *filename, time_t expiry) {
         fprintf(fp, "%s %ld\n", filename, expiry);
         fclose(fp);
     }
-
-
-    free(task);
-    return NULL;
-}
-
-void* handle_client(void* arg) {
-    int client_sock = *((int*)arg);
-    free(arg);
-
-    char filename[MAX_FILENAME];
-    int filename_len, file_size, expiry;
-
-    // Receive filename length
-    if (recv(client_sock, &filename_len, sizeof(int), 0) <= 0) return NULL;
-    if (recv(client_sock, filename, filename_len, 0) <= 0) return NULL;
-    filename[filename_len] = '\0';
-
-    // Receive file size
-    if (recv(client_sock, &file_size, sizeof(int), 0) <= 0) return NULL;
-
-    // Receive expiry time in seconds
-    if (recv(client_sock, &expiry, sizeof(int), 0) <= 0) return NULL;
-
-    // Save the file
-    FILE* fp = fopen(filename, "wb");
-    if (!fp) {
-        perror("[-] File open failed");
-        close(client_sock);
-        return NULL;
-    }
-
-    char buffer[BUFFER_SIZE];
-    int bytes_received = 0;
-    while (bytes_received < file_size) {
-        int bytes = recv(client_sock, buffer, BUFFER_SIZE, 0);
-        if (bytes <= 0) break;
-        fwrite(buffer, 1, bytes, fp);
-        bytes_received += bytes;
-    }
-
-    fclose(fp);
-    printf("[+] Received: %s (%d bytes), expires in %d seconds\n", filename, bytes_received, expiry);
-
-    // Schedule deletion
-    FileExpiryTask* task = malloc(sizeof(FileExpiryTask));
-    strcpy(task->filename, filename);
-    task->expiry_seconds = expiry;
-
-    pthread_t tid;
-    pthread_create(&tid, NULL, delete_file_after_delay, (void*)task);
-    pthread_detach(tid);
-
-    close(client_sock);
-    return NULL;
-
 }
 
 int main() {
